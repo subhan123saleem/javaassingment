79d0d3eacc1a4cc20de8a1d3bcbe6f5564421073
subhan123saleem <kishwarkawalvlogs@gmail.com>
2024-09-19 14:31:34 +0500

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,24 @@
+var user=prompt('Enter your name');
+var name='HUSBAN';
+if (user == name) {
+    document.write(name)
+} else {
+    document.write("Enter write name?")
+}
+
+// Q.NO:2
+
+var fruits=[''];
+
+// Q.NO:3;
+
+var num = [''];
+
+// Q.NO:4;
+
+var mixed=["HUSBAN",100];
+
+// Q.NO:5;
+
+var qualification=['SSC <br>', 'HSC <br>', 'BSC <br>','BS <br>', 'BCOM <br>', 'MS <br>' , 'M.Phil. <br>' , 'PHD <br>'];
+document.write(qualification);
